497cb725d54e06ffaa4bd4d6907bff70fb4f4a5f
jeowon_1 <rlawjddnsj8371@gmail.com>
2025-01-12 01:21:04 +0900

    test

---
+++ src/main/java/com/example/demo/controller.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class controller {
+
+    @GetMapping("/get")
+    public String get() {
+        return "Hello World";
+    }
+}
